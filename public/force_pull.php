6eb70ab30f3de3b628fd72fc5a4266d8f2c0e0a4
Chillocreative <chillocreative@gmail.com>
2026-02-05 06:30:27 +0800

    Cleanup debug files and consolidate deployment workflow

--- public/force_pull.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-echo "FORCE PULL START\n";
-$output = [];
-$return_var = 0;
-exec("cd .. && git fetch origin main 2>&1", $output);
-exec("cd .. && git reset --hard origin/main 2>&1", $output);
-echo implode("\n", $output);
-echo "\nFORCE PULL END\n";
--- public/view_logs.php
+++
@@ -1,8 +0,0 @@
-<?php
-$logFile = __DIR__.'/../storage/logs/laravel.log';
-if (file_exists($logFile)) {
-    header('Content-Type: text/plain');
-    echo shell_exec("tail -n 100 " . escapeshellarg($logFile));
-} else {
-    echo "Log file not found: " . $logFile;
-}
