6dce343205669096b4532895465cc9cb7d2fd80f
Chillocreative <chillocreative@gmail.com>
2026-02-05 05:25:00 +0800

    Add PHP proxy for Node logs

---
+++ public/view_node_logs.php
@@ -0,0 +1,11 @@
+<?php
+$logFile = __DIR__.'/whatsapp-server-temp/server.log';
+if (file_exists($logFile)) {
+    header('Content-Type: text/plain');
+    echo file_get_contents($logFile);
+} else {
+    echo "Node log not found at: " . $logFile . "\n";
+    // Try to list files in the folder
+    echo "FILES IN whatsapp-server-temp:\n";
+    print_r(scandir(__DIR__.'/whatsapp-server-temp'));
+}
