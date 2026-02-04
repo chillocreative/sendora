7082f5b488523beac898428bb83e75cb49b342b9
Chillocreative <chillocreative@gmail.com>
2026-02-05 05:45:18 +0800

    Explore public dir

--- public/explorer.php
+++ public/explorer.php
@@ -3,10 +3,5 @@ header('Content-Type: text/plain');
 echo "ROOT DIR: " . realpath(__DIR__.'/..') . "\n";
 echo "FILES IN ROOT:\n";
 print_r(scandir(__DIR__.'/..'));
-echo "\nREADING whatsapp-server-temp/server.log:\n";
-$log = __DIR__.'/../whatsapp-server-temp/server.log';
-if (file_exists($log)) {
-    echo file_get_contents($log);
-} else {
-    echo "Log file NOT FOUND at $log\n";
-}
+echo "\nFILES IN PUBLIC:\n";
+print_r(scandir(__DIR__));
