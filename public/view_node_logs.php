3854bb391a9215313d24bae8a8624a4682baba57
Chillocreative <chillocreative@gmail.com>
2026-02-05 05:30:12 +0800

    Fix path in Node log viewer

--- public/view_node_logs.php
+++ public/view_node_logs.php
@@ -1,5 +1,5 @@
 <?php
-$logFile = __DIR__.'/whatsapp-server-temp/server.log';
+$logFile = __DIR__.'/../whatsapp-server-temp/server.log';
 if (file_exists($logFile)) {
     header('Content-Type: text/plain');
     echo file_get_contents($logFile);
@@ -7,5 +7,5 @@ if (file_exists($logFile)) {
     echo "Node log not found at: " . $logFile . "\n";
     // Try to list files in the folder
     echo "FILES IN whatsapp-server-temp:\n";
-    print_r(scandir(__DIR__.'/whatsapp-server-temp'));
+    print_r(scandir(__DIR__.'/../whatsapp-server-temp'));
 }
